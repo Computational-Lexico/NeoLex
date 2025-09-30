4cb832e00a01a3286355425a6e25ff33ee5f8dbd
Computational-Lexico <182833204+Computational-Lexico@users.noreply.github.com>
2025-09-30 18:10:34 +0200

    Update app_chi.py

--- app_chi.py
+++ app_chi.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 
 # Charger la liste des néologismes depuis le fichier JSON
-df = pd.read_json("/Users/lianchen/Desktop/CNRS/Stage-CNRS-ANR/STage5月-9月/实习报告/可视化/neo_chi.json")
-
+#df = pd.read_json("/Users/lianchen/Desktop/CNRS/Stage-CNRS-ANR/STage5月-9月/实习报告/可视化/neo_chi.json")
+df = pd.read_json("neo_chi.json")
 st.set_page_config(page_title="Dictionnaire des néologismes", layout="wide")
 
 st.title("Petit dictionnaire des néologismes du chinois moderne")
